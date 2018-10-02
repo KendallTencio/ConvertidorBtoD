39c96978f71b27276c1beb614f697df3e2dd35e3
KendallTencio <kendalltencio132e@gmail.com>
2018-10-02 01:26:24 -0600

    Decimal a Binario Funcional

--- main.cpp
+++ main.cpp
@@ -9,6 +9,10 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    Convertidor g;
+    g.decimalTobinario(2);
+    std::cout<<g.binarioToDecimal(g.getListaEnteros())<<std::endl;
+
 
     return a.exec();
 }
